33acaa5ae2e27641538f267ea9f42bbaeaa05596
Gautham P Harish <90624687+Gauthampharish@users.noreply.github.com>
2023-12-06 15:07:15 +0530

    Update astro.config.mjs
    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
-	site:'https:gphanswers.me/'
+	site:'https://gphanswers.me'
 });
